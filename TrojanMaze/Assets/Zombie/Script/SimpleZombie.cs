e61832c542d2be7d6d954c5acd4895436029b106
Ventues <ventues7@gmail.com>
2022-02-06 17:28:18 -0800

    Basic zombie model that avoids hitting wall

--- TrojanMaze/Assets/Zombie/Script/SimpleZombie.cs
+++ TrojanMaze/Assets/Zombie/Script/SimpleZombie.cs
@@ -8,24 +8,45 @@ public class SimpleZombie : Zombie {
 
     private Rigidbody2D _body;
     private Vector2 _direction;
+
+    private GameObject prevHit;
+
+    private bool turnClockWise;
     // Start is called before the first frame update
     void Start() {
         base.Init(InitialHealth, InitialSpeed);
         _body = GetComponent<Rigidbody2D>();
+        turnClockWise = false;
     }
 
     // Update is called once per frame
     void Update() {
         Vector2 movement = transform.right * Speed * Time.deltaTime;
-        Debug.Log(transform.forward);
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position + transform.right, 0.15f, transform.right);
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position + transform.right, 0.5f, transform.right);
+
         if(hit) {
             Debug.Log(hit.collider.gameObject.tag);
-            if(hit.distance < 2.0f) {
-                float angle = Random.Range(-30, 30);
-                _body.MoveRotation(angle*2*Time.deltaTime);
+            if(hit.distance < 1.0f && !hit.collider.gameObject.CompareTag("Player")) {
+                _body.velocity = new Vector2(0, 0);
+                float angle = Random.Range(0, 70);
+
+                float randomAction = Random.Range(0, 10);
+                if(hit.collider.gameObject != prevHit && randomAction < 5) {
+                    turnClockWise = !turnClockWise;
+                }
+                if(turnClockWise) {
+                    angle *= -1;
+                }
+
+                Quaternion endRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + angle);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, endRotation, 500 * Time.deltaTime);
+                prevHit = hit.collider.gameObject;
+            } else {
+                _body.velocity = movement;
             }
+        } else {
+            _body.velocity = movement;
         }
-        //_body.velocity = movement;
+        
     }
 }
