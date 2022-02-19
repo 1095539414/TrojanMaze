563da0faa2c17540261ee0231f4ca36b340edbb8
Ventues <ventues7@gmail.com>
2022-02-19 12:27:23 -0800

    Balanced virus zone a little. Made the virus zone do constant damage

--- TrojanMaze/Assets/Zombie/Script/VirusZone.cs
+++ TrojanMaze/Assets/Zombie/Script/VirusZone.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VirusZone : MonoBehaviour
-{
+public class VirusZone : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField] float ValidityPeriod = 1f;
     private float _time = 0f;
 
-    [SerializeField] float virusDamage = 0.02f;
-    void Start()
-    {
-        
+    [SerializeField] float virusDamage = 0.002f;
+
+    float _damageInterval = 0.1f;
+    float _damageTime = 0f;
+    GameObject _damageTarget;
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if(_damageTarget) {
+            _damageTime += Time.deltaTime;
+            if(_damageTime >= _damageInterval) {
+                iDamageable damageableObj = _damageTarget.gameObject.GetComponent<iDamageable>();
+                if(damageableObj != null) {
+                    damageableObj.ReduceHealth(virusDamage);
+                }
+                _damageTime = 0f;
+            }
+        }
         _time += Time.deltaTime;
         if(_time > ValidityPeriod) {
             Destroy(gameObject);
@@ -25,10 +36,13 @@ public class VirusZone : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            iDamageable damageableObj = other.gameObject.GetComponent<iDamageable>();
-            if(damageableObj != null) {
-                damageableObj.ReduceHealth(virusDamage);
-            }
+            _damageTarget = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            _damageTarget = null;
         }
     }
 }
