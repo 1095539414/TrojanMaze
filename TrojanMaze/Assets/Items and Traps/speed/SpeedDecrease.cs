1bfba6af0d8beca84e07b32cad302f7c4a451a11
Ventues <ventues7@gmail.com>
2022-02-19 22:30:48 -0800

    Added a larger capsule trigger for simple zombie to better detect bullets

--- TrojanMaze/Assets/Items and Traps/speed/SpeedDecrease.cs
+++ TrojanMaze/Assets/Items and Traps/speed/SpeedDecrease.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 /// <summary>
 ///speed decrease
 /// </summary>
-public class SpeedDecrease : MonoBehaviour
-{
+public class SpeedDecrease : MonoBehaviour {
     public float time = 3f;
     float newMove = 100f;
 
@@ -15,23 +14,19 @@ public class SpeedDecrease : MonoBehaviour
     /// </summary>
     public static SpeedDecrease _speedDecrease;
 
-    private void Awake()
-    {
+    private void Awake() {
         _speedDecrease = this;//static this scirpts for other scripts to deploy
     }
 
 
 
 
-    private void Update()
-    {
+    private void Update() {
         Debug.Log(Move._move.speed);
     }
-    void OnTriggerEnter2D(Collider2D other)
-    {
+    void OnTriggerEnter2D(Collider2D other) {
         //Destroy it when player collide it
-        if (other.gameObject.CompareTag("Player"))
-        {
+        if(other.gameObject.CompareTag("Player")) {
 
             Move._move.speed = newMove;
             Invoke("oldmove", time);// // deploy this to return to the old speed in 3s
@@ -43,8 +38,7 @@ public class SpeedDecrease : MonoBehaviour
         }
     }
 
-    void oldmove()
-    {
+    void oldmove() {
         newMove = 300f;
         Move._move.speed = newMove;
         Destroy(gameObject);//destroy the item after 3s
