9dbe480f1236b5ba66505dcaf9b310f7a373ecfe
Ventues <ventues7@gmail.com>
2022-02-28 16:38:07 -0800

    Reduced trap damage to 0.25f

--- TrojanMaze/Assets/Items and Traps/Script/HpReduce.cs
+++ TrojanMaze/Assets/Items and Traps/Script/HpReduce.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HpReduce : MonoBehaviour {
-    private float _damage = 0.5f;
+    private float _damage = 0.25f;
     // Start is called before the first frame update
     void Start() {
 
