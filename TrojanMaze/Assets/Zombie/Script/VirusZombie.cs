3ae8fdbc41182660081bf4d512017a4316a4b592
binji <binji@usc.edu>
2022-03-06 18:26:35 -0800

    (update):virus zombies can move vertically

--- TrojanMaze/Assets/Zombie/Script/VirusZombie.cs
+++ TrojanMaze/Assets/Zombie/Script/VirusZombie.cs
@@ -6,13 +6,14 @@ public class VirusZombie : Zombie {
     [SerializeField] int initialHealth = 2;
     [SerializeField] float moveSpeed = 0.02f;
     [SerializeField] float releaseInterval = 0.2f;
+    [SerializeField] bool horizontal = true;
 
     [SerializeField] GameObject VirusZone;
     [SerializeField] Transform center;
     private Rigidbody2D _body;
     private GameObject _player;
     private Vector2 _initialPos;
-    private bool _moveRight = true;
+    private bool _direction = true;
     private float _time = 0f;
 
     void Start() {
@@ -24,15 +25,31 @@ public class VirusZombie : Zombie {
     // Update is called once per frame
     void Update() {
         _time += Time.deltaTime;
-        if(_moveRight) {
-            _body.MovePosition(transform.position + Vector3.right * moveSpeed);
-            if(transform.position.x - _initialPos.x > 3f) {
-                _moveRight = false;
+        // virus zombies move horizontally 
+        if(horizontal) {
+            if(_direction) {
+                _body.MovePosition(transform.position + Vector3.right * moveSpeed);
+                if(transform.position.x - _initialPos.x > 3f) {
+                    _direction = false;
+                }
+            } else {
+                _body.MovePosition(transform.position + Vector3.left * moveSpeed);
+                if(transform.position.x - _initialPos.x < -3f) {
+                    _direction = true;
+                }
             }
+        // virus zombies move vertically 
         } else {
-            _body.MovePosition(transform.position + Vector3.left * moveSpeed);
-            if(transform.position.x - _initialPos.x < -3f) {
-                _moveRight = true;
+            if(_direction) {
+                _body.MovePosition(transform.position + Vector3.up * moveSpeed);
+                if(transform.position.y - _initialPos.y > 3f) {
+                    _direction = false;
+                }
+            } else {
+                _body.MovePosition(transform.position + Vector3.down * moveSpeed);
+                if(transform.position.y - _initialPos.y < -3f) {
+                    _direction = true;
+                }
             }
         }
         if(_time >= releaseInterval) {
