cab83196a8c795593e4dce7aced6b744cf08917a
JYL <jyl@JYL-MBA.local>
2022-04-04 16:24:27 -0700

    fix tower

--- TrojanMaze/Assets/Items and Traps/Script/Tower.cs
+++ TrojanMaze/Assets/Items and Traps/Script/Tower.cs
@@ -5,51 +5,45 @@ using UnityEngine;
 public class Tower : MonoBehaviour {
     private Camera myCamera;
     float initialCamera;
-    float boostCamera = 10f;
+    float boostCamera = 8f;
     float cameraZoom;
-    bool activate;
+    float cameraZoomSpeed = 0.1f;
     // Start is called before the first frame update
     void Start() {
         myCamera = Camera.main;
         initialCamera = myCamera.orthographicSize;
         cameraZoom = initialCamera;
-        activate = false;
     }
 
     // Update is called once per frame
     void Update() {
         zoom();
+        Debug.Log(myCamera.orthographicSize);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player") {
-            activate = true;
+        if(other.tag == "Player") {  
             cameraZoom = boostCamera;
+            StartCoroutine(zoom());
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if(other.tag == "Player") {
             cameraZoom = initialCamera;
+            StartCoroutine(zoom());
         }
     }
 
-    private void zoom() {
-        if(!activate) return;
-        float cameraZoomDifference = cameraZoom - myCamera.orthographicSize;
-        float cameraZoomSpeed = 1f;
-
-        myCamera.orthographicSize += cameraZoomDifference * cameraZoomSpeed * Time.deltaTime;
-
-        if(cameraZoomDifference > 0) {
-            if(myCamera.orthographicSize > cameraZoom) {
-                myCamera.orthographicSize = cameraZoom;
+    IEnumerator zoom(){
+        float cameraZoomDifference = Mathf.Abs(cameraZoom - myCamera.orthographicSize);
+        while(myCamera.orthographicSize != cameraZoom) {
+            if (myCamera.orthographicSize < cameraZoom) {
+                myCamera.orthographicSize +=  cameraZoomSpeed * Time.deltaTime;
+            } else {
+                myCamera.orthographicSize -=  cameraZoomSpeed * Time.deltaTime; 
             }
-        } else {
-            if(myCamera.orthographicSize < cameraZoom) {
-                myCamera.orthographicSize = cameraZoom;
-                activate = false;
-            }
+            yield return new WaitForSeconds (0.0f); 
         }
     }
 }
