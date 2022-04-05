78c1d8e03afd19dcde0312d6ab1f2bf1350b540b
Ventues <ventues7@gmail.com>
2022-04-04 20:40:32 -0700

    Further fixed player rotation with sword

--- TrojanMaze/Assets/Player/Script/sword.cs
+++ TrojanMaze/Assets/Player/Script/sword.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class sword : MonoBehaviour {
 
-    float swordDamage = 0.2f;
+    float swordDamage = 0.5f;
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Zombie") && !other.isTrigger) {
