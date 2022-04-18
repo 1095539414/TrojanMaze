040a5b74590c4809473e1b25ea887adbf76578bd
binji <binji@usc.edu>
2022-04-18 13:20:44 -0700

    add Num of killed Zombies

---
+++ TrojanMaze/Assets/Utility/Script/ScoreScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TMP;
+
+    private static int KillNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP.text = "Killed Zombies: 0"; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         TMP.text = "Killed Zombies: " + KillNum; 
+    }
+
+    public static void IncreaseKillNum() {
+        KillNum++;
+    }
+}
