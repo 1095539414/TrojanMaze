5103222541d3742884e04008e7dbf94def3ee81e
Ventues <ventues7@gmail.com>
2022-04-18 23:12:59 -0700

    Modifying menu

---
+++ TrojanMaze/Assets/Zombie/Script/MenuAI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class MenuAI : MonoBehaviour {
+    private Rigidbody2D _body;
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
