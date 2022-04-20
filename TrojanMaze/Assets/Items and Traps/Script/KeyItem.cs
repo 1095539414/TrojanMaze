10e7c4f47368cd526b217e920063a453a730699d
Ventues <ventues7@gmail.com>
2022-04-20 14:41:05 -0700

    Spike show in scene as well

--- TrojanMaze/Assets/Items and Traps/Script/KeyItem.cs
+++ TrojanMaze/Assets/Items and Traps/Script/KeyItem.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyItem : MonoBehaviour
-{
+public class KeyItem : MonoBehaviour {
     private GameObject buffTarget;
     public GameObject trailPanel;
 
+    private void Start() {
+        gameObject.layer = 7;
+
+    }
     protected void OnTriggerEnter2D(Collider2D other) {
 
         buffTarget = other.gameObject;
@@ -15,12 +18,12 @@ public class KeyItem : MonoBehaviour
         }
 
         if(other.CompareTag("Player")) {
-             if(trailPanel != null) {
+            if(trailPanel != null) {
                 trailPanel.SetActive(true);
             }
             State.setKeyState(true);
             Debug.Log("Hello");
             this.gameObject.SetActive(false);
-        } 
+        }
     }
 }
