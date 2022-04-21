6e3708dd1fb32bc9618bfa6015eeb539b32323cb
1095539414 <yueqiuyang1996@gmail.com>
2022-04-20 18:01:47 -0700

    Fix bulletNum bugs

---
+++ TrojanMaze/Assets/Items and Traps/Script/GunItem1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunItem1 : BuffItem {
+    // Start is called before the first frame update
+
+    private int bulletNum = 20;
+    
+    protected override bool AddBuff() {
+     //   buffTarget.GetComponent<Move>().EnableGun(this.gameObject);
+        return true;
+    }
+
+    protected override bool RemoveBuff() {
+        return false;
+    }
+
+    protected override float GetDuration() {
+        return 0f;
+    }
+
+    public int getBulletNum()
+    {
+        return bulletNum;
+    }
+
+    public void shoot()
+    {
+        bulletNum--;
+    }
+}
