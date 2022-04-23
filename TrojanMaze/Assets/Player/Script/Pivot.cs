7702ce8120583467f60ca421b3dd0e887cba75fb
Ventues <ventues7@gmail.com>
2022-04-23 15:53:16 -0700

    Fix gun/sword bug

--- TrojanMaze/Assets/Player/Script/Pivot.cs
+++ TrojanMaze/Assets/Player/Script/Pivot.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Pivot : MonoBehaviour {
 
+    private Camera _camera;
     public GameObject myPlayer;
     Rigidbody2D rigidBody;
 
+    private void Start() {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
     private void FixedUpdate() {
-        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3 diff = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         diff.Normalize();
 
         float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
